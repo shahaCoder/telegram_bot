2d5458a0263222d33542a27e879c1b37e87e8a72
Shoxrux Code <shoxruxpl04@gmail.com>
2023-01-28 19:20:11 +0500

    added: products

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import { useEffect } from 'react';
 import './App.css';
-const tg = window.Telegram.WebApp
-function App() {
+import Products from './Components/Products';
 
-  useEffect(() => {
-      tg.ready()
-  }, []);
-  const handleClose = () => {
-    tg.close()
-  }
+function App() {
+  const arr = [
+    {
+      id: Math.random(),
+      name: 'HQD melo 1000 💨',
+      desc: '✨Кокосовое молокоананас🥥',
+      desc2: '⚡️Энергетик🏃🏼‍♀️',
+      type: '🔹Черника🫐',
+      type2: '🔺Арбуз🍉',
+      type3: '🔻Малина🤭',
+      price: '200💸',
+      img: '/images/num1.jpg'
+    },
+    {
+      id: Math.random(),
+      name: 'ELF BAR 5000💨',
+      desc: '🟠тройная дыня 🍈',
+      desc2: '🔴арбузная резинка 🍉',
+      type: '🟡клубничный банан 🍌',
+      type2: '⚪️лимон мята 🍋',
+      type3: '🟣красный мохито 🍹',
+      price: '700💸',
+      img: '/images/num1.jpg'
+    },
+    {
+      id: Math.random(),
+      name: 'Морс 🌬️',
+      desc: '🔴 черника смородина 🫐',
+      desc2: '🟠персик груша 🍐',
+      type: '🟡малина земляника 🍓',
+      type2: '🟢яблоко черника 🍎',
+      type3: '🔵лесные ягоды 🏕️',
+      price: '450💸',
+      img: '/images/num1.jpg'
+    },
+    {
+      id: Math.random(),
+      name: 'Hot Spot 50mg',
+      desc: '🔴лимон брусника 🍋',
+      desc2: '🟠яблоко груша 🍐',
+      type: '🟡ананас ежевика🍍',
+      type2: '🟢черника дыня 🍈',
+      type3: '🔵арбуз жвачка 🍉',
+      price: '450💸',
+      img: '/images/num1.jpg'
+    }
+  ]
   return (
     <div>
-         work
-         <button onClick={handleClose}>Close</button>
-         <span>
-             {tg.initDataUnsafe?.user?.username}
-         </span>
+      <div>
+         <div>
+            <Products data={arr} />
+         </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Products.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import Item from './child/Item';
+
+const tg = window.Telegram.WebApp
+const Products = ({data}) => {
+    useEffect(() => {
+        tg.ready()
+    }, []);
+    const handleClose = () => {
+      tg.close()
+    }
+    return (
+        <div className='w-[90%] m-auto'>
+        <h1 className='mb-6 mt-2'>Привет, <span>
+             {tg.initDataUnsafe?.user?.username}
+         </span></h1>
+        <div className='w-full  flex flex-wrap gap-2'>
+            {
+                data.map(i => <Item key={i.id} item={i} />)
+            }
+        </div>
+        </div>
+    );
+}
+
+export default Products;
