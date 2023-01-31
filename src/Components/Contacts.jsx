22eccc52a723b2fbc97ac79ca98e6dcfe2069cd4
Shoxrux Code <shoxruxpl04@gmail.com>
2023-01-31 17:38:30 +0500

    smth

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Contacts = () => {
     return (
         <div className='w-[90%] m-auto'>
-            <h1>Контакты</h1>
-            <p>89141069216 - Кирилл</p>
-            <p>89142952274 - Богдан</p>
+            <h1 className='text-gray-500'>Контакты</h1>
+            <p className='text-gray-500'>89141069216 - Кирилл</p>
+            <p className='text-gray-500'>89142952274 - Богдан</p>
         </div>
     );
 }
